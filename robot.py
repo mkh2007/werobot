467902af2a610e580a312802b758494df86bfd71
mht <mht@upapp.com>
2015-12-15 17:05:52 +0800

    bug

--- robot.py
+++ robot.py
@@ -3,8 +3,13 @@ import werobot
 
 robot = werobot.WeRoBot(token='abcdefghijklmn', enable_session=False)
 
+application = robot.wsgi
+
+@robot.filter('a')
+def echo(message):
+	return u'你好'
+
 @robot.handler
 def echo(message):
     return 'Hello World!'
 
-application = robot.wsgi

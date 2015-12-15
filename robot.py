a03553f9b1ebdb1dd582765a8b2d87df39624e45
mht <mht@upapp.com>
2015-12-15 14:18:13 +0800

    init

---
+++ robot.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import werobot
+
+robot = werobot.WeRoBot(token='abcdefghijklmn', enable_session=True)
+
+
+@robot.handler
+def echo(message):
+    return 'Hello World!'
+
+robot.run()
